2d28cc59aacdb4d3f77431682f67b000a0675026
A <99933422+data-sandbox@users.noreply.github.com>
2023-07-16 17:07:55 -0600

    Add decimal and delete features

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
 const equalButton = document.querySelector('.equal');
 const clearButton = document.querySelector('.clear');
+const deleteButton = document.querySelector('.delete');
+const decimalButton = document.querySelector('.decimal');
+const percentButton = document.querySelector('.percent');
 
 function clearDisplay() {
     displayValue = '';
@@ -19,17 +22,14 @@ function appendNumber(e) {
         clearDisplay();
         clearDisplayFlag = false;
     }
-    console.log(this.id); // debug
+    // console.log(this.id);
     let number = this.id;
     displayValue += number;
     document.getElementById('display-value').textContent = displayValue;
-
-    console.log({ firstNum }, { secondNum });
 }
 
 function storeOperator(e) {
-    console.log(this.id); // debug
-    console.log({ firstNum }, { secondNum });
+    // console.log({ firstNum }, { secondNum });
     if (!firstNum) {
         operator = this.id;
         firstNum = displayValue;
@@ -54,28 +54,68 @@ function clearValues() {
     firstNum = null;
     secondNum = null;
     operator = null;
-    displayValue = '';
     clearDisplayFlag = false;
-    console.log({ firstNum }, { secondNum });
 }
 
-function calcEquation() {
-    secondNum = displayValue;
-    let result = calcValue(operator, firstNum, secondNum);
+// TODO
+// function countDecimals(number) {
+//     if (Math.floor(number) === number) return 0;
+//     return toString(number).split(".")[1].length || 0;
+// }
+
+// function round(result) {
+//     let precisionLimit = 7;
+//     if (typeof result === 'number' && Number.isInteger(result)) {
+//         return result;
+//     } else if (countDecimals(result) > precisionLimit) {
+//         return result.toFixed(precisionLimit);
+//     } else {
+//         return result;
+//     }
+// }
+
+function displayResult(result) {
+    // result = round(result);
     document.getElementById('equation-value').textContent = ' ';
     document.getElementById('display-value').textContent = result;
     clearValues();
+    displayValue = result;
+    clearDisplayFlag = true;
+}
+
+function calcEquation() {
+    if (!firstNum) {
+        firstNum = displayValue;
+        displayResult(Number(firstNum));
+    } else {
+        secondNum = displayValue;
+        let result = calcValue(operator, firstNum, secondNum);
+        displayResult(result);
+    }
 }
 
 function clearAll() {
     clearValues();
+    displayValue = '';
     document.getElementById('display-value').textContent = 0;
     document.getElementById('equation-value').textContent = null;
 }
 
+// TODO
+// function convertPercent(value) {
+//     value = toString(value)
+//     if (value.at(-1) === '%') {
+//         value = value.substring(0, value.length - 1);
+//         return Number(value) / 100;
+//     } else {
+//         return Number(value);
+//     }
+// }
 
 function calcValue(operator, a, b) {
     console.log({ operator }, { a }, { b });
+    // a = Number(convertPercent(a));
+    // b = Number(convertPercent(b));
     a = Number(a);
     b = Number(b);
     switch (operator) {
@@ -93,7 +133,28 @@ function calcValue(operator, a, b) {
     }
 }
 
+function deleteChar() {
+    if (displayValue) {
+        // TODO: displayValue might be 'number'
+        displayValue = displayValue.substring(0, displayValue.length - 1);
+        document.getElementById('display-value').textContent = displayValue;
+    }
+}
+
+function insertDecimal() {
+    displayValue += '.';
+    document.getElementById('display-value').textContent = displayValue;
+}
+
+function insertPercent() {
+    displayValue += '%';
+    document.getElementById('display-value').textContent = displayValue;
+}
+
 numberButtons.forEach(button => button.addEventListener('click', appendNumber));
 operatorButtons.forEach(button => button.addEventListener('click', storeOperator));
 equalButton.addEventListener('click', calcEquation);
 clearButton.addEventListener('click', clearAll);
+deleteButton.addEventListener('click', deleteChar);
+decimalButton.addEventListener('click', insertDecimal);
+// percentButton.addEventListener('click', insertPercent);
